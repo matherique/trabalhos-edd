a5da8830cb0cdd62f3f2965ddfa3204f7c5f745a
Matheus Henrique <matherique@gmail.com>
2019-12-14 21:05:50 -0300

    update rbt

--- trabalho_final/RBT.py
+++ trabalho_final/RBT.py
@@ -1,146 +1,40 @@
 #!/usr/bin/env python3
+
 class Node:
-    def __init__(self, key=None, value=None):
+    def __init__(self, value):
         self.left = None
         self.right = None
-        self.parent = None
-        self.key = key
         self.value = value
-        self.ifLeftChild = False
-        self.black = False
+        self.parent = None
 
-class RBD:
+    def __str__(self):
+        return f"{self.value}"
+
+    
+
+class Tree:
+    
     def __init__(self):
         self.root = None
-        self.size = 0
-    
-    def insert(self, key, value):
-        node = Node(key, value)
+        self.space = 0
 
+    def add(self, value):
+        # tree is empty
         if self.root == None:
-            self.root = node
-            self.root.black = True
-            self.size += 1
+            self.root = Node(value)
             return 
-
-        add(root, node)
-
-    def add(self, parent, newNode):
-        if parent.key == newNode.key:
-            if parent.right == None:
-                parent.right = newNode
-                newNode.parent = parent
-                newNodee.ifLeftChild = False
-
-                return
-
-            return add(parent.right, newNode)
-
-        if parent.left == None:
-            parent.left = newNode
-            newNode.parent = parent
-            newNodee.ifLeftChild = True 
-            return
-
-        checkColor(newNode)    
-        return add(parent.left, newNode)
-
-    def checkColor(self, node):
-        if node == self.root:
-            return
-
-        if not node.black and not node.parent.black:
-            correct(node)
-            
-        checkColor(node.parent)
-
-    def corrent(self, node):
-        if node.parent.isLeftChild:
-            if node.parent.parent.right == None or node.parent.parent.right.black:
-                return rotation(node)
-            
-            if node.parent.parent.right != None:
-                node.parent.parent.right.black = True
-
-            node.parent.parent.black = False
-            node.parent.black = True
-
-            return
-
-        if node.parent.parent.left == None or node.parent.parent.left.black:
-            return rotation(node)
-            
-        if node.parent.parent.left != None:
-            node.parent.parent.left.black = True
-
-        node.parent.parent.black = False
-        node.parent.black = True
-
-        return
-
-    
-    def rotation(self, node):
-        if node.isLeftChild:
-            if node.parent.isLeftChild:
-                rightRotation(node.parent.parent)
-                node.black = False
-                node.parent.black = True
-
-                if node.parent.right != None:
-                    node.parent.right.black = False
-
-                return 
-            rightLeftRotation(node.parent.parent)
-            node.black = True
-            node.right.black = False
-            node.left.black = False
-
-            return 
-
-
-        if not node.parent.isLeftChild:
-            leftRotation(node.parent.parent)
-            node.black = False
-            node.parent.black = True
-
-            if node.parent.right != None:
-                node.parent.right.black = False
-
-            return 
-        leftRightRotation(node.parent.parent)
-
-        node.black = True
-        node.right.black = False
-        node.left.black = False
-
-        return 
-
-
-    def leftRotation(self, node):
-        temp = node.right
-        node.right = temp.left
         
-        if node.right != None:
-            node.right.parent = node
-            node.right.isLeftChild = False
+        if value > self.root.value:
+            self.root.right = Node(value)
+            return 
+        self.root.left = Node(value)
 
-        if node.parent == None:
-            self.root = temp
-            temp.parent = None
-        else:
-            temp.parent = node.parent
-            if node.isLeftChild:
-                temp.isLeftChild = True
-                temp.parent.left = temp
-            else:
-                temp.isLeftChild = False 
-                temp.parent.right = temp 
-
-
-        temp.left = node
-        node.isLeftChild = True
-        node.parent = temp
-
+    def __str__(self):
+        
 
 if __name__ == '__main__':
-    print("ola mundo")
+    t = Tree()
+    t.add(10)
+    t.add(12)
+    t.add(5)
+    print(f"{t}")
