5de196886526f005f7a28185e2cb88bad24859bf
Matheus Henrique <matherique@gmail.com>
2019-12-14 21:06:45 -0300

    update rbt

--- trabalho_final/RBT.py
+++ trabalho_final/RBT.py
@@ -10,31 +10,3 @@ class Node:
     def __str__(self):
         return f"{self.value}"
 
-    
-
-class Tree:
-    
-    def __init__(self):
-        self.root = None
-        self.space = 0
-
-    def add(self, value):
-        # tree is empty
-        if self.root == None:
-            self.root = Node(value)
-            return 
-        
-        if value > self.root.value:
-            self.root.right = Node(value)
-            return 
-        self.root.left = Node(value)
-
-    def __str__(self):
-        
-
-if __name__ == '__main__':
-    t = Tree()
-    t.add(10)
-    t.add(12)
-    t.add(5)
-    print(f"{t}")
